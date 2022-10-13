ed5b63517d821b9c147bea353b6f5eca58320e68
John <yhonelvis49@gmail.com>
2022-10-12 21:24:27 -0400

    envio de prueba

---
+++ listas.py
@@ -0,0 +1,7 @@
+text = input("ingrese texto: ")
+if "hola" in text:
+    print("costa debe 0$")
+elif "Hola" in text:
+    print("costa debe 0$")
+else:
+    print("costa debe 10$ por no ser educado")
